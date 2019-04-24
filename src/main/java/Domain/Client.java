b394c3e0581d7d67e107513b5ced15ad94b65184
Robson Adorno <rferreira@scopus.com.br>
2019-04-24 16:07:24 -0300

    Mapeando as entidades

---
+++ src/main/java/Domain/Client.java
@@ -0,0 +1,47 @@
+package Domain;
+
+import Enums.RequestStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 75, nullable = false)
+    private String name;
+
+    @Column(length = 11, nullable = false, unique = true)
+    private String cpf;
+
+    @Column(length = 75, nullable = false, unique = true)
+    private String login;
+
+    @Column(length = 100, nullable = false)
+    private String senha;
+
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RequestStatus role;
+
+    @OneToMany(targetEntity = Request.class, fetch=FetchType.EAGER)
+    @JoinColumn(name="request_id")
+    private List<Request> requests = new ArrayList<Request>();
+
+    @OneToMany(targetEntity = RequestStage.class, fetch=FetchType.EAGER)
+    @JoinColumn(name="request_id")
+    private List<RequestStage> userStage = new ArrayList<RequestStage>();
+
+}
--- src/main/java/Domain/RequestStage.java
+++ src/main/java/Domain/RequestStage.java
@@ -5,16 +5,36 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "request_stage")
 public class RequestStage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "text")
     private String description;
+
+    @Column(name = "realization_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date realizationDate;
+
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
     private RequestStatus state;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id", nullable = false)
     private Request request;
-    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
 }
