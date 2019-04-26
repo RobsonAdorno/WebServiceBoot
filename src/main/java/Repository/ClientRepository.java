625273f17e982f6142c291b86f847386e24c55b5
Robson Adorno <rferreira@scopus.com.br>
2019-04-26 17:08:52 -0300

    [WebServiceBoot] Criando repository para o client

--- src/main/java/Repository/ClientRepository.java
+++ src/main/java/Repository/ClientRepository.java
@@ -1,5 +1,15 @@
 package Repository;
-public interface ClientRepository {
 
+import Domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ClientRepository extends JpaRepository<ClientRepository, Integer> {
+
+    @Query("SELECT FROM Client WHERE login =?1 AND senha = ?2")
+    public boolean isLoginSucessfull(String login, String senha);
+
+    public Client findClientByCPF(String cpf);
 }
